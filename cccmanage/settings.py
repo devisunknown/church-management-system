a4d5018bde9a0d5c139d47047f1e06c7d9ffce6c
devisknown <starblegamer@gmail.com>
2026-07-25 17:56:49 +0000

    added postgres database

--- cccmanage/settings.py
+++ cccmanage/settings.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+from dotenv import load_dotenv
+load_dotenv()
 
 from pathlib import Path
 import os
 import dj_database_url
-
+from dotenv import load_dotenv 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
